20c7216c7c8f5c12164d011d1c1fbadd9d9bd20d
Oleg Dugaev <dugaevoleg9@gmail.com>
2024-02-06 13:45:57 +0200

    app init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import VideoCarousel from "./components/VideoCarousel";
+
+const App = () => {
+  const videos = [
+    "https://player.vimeo.com/video/824804225",
+    "https://player.vimeo.com/video/824804225",
+    "https://player.vimeo.com/video/824804225",
+    "https://player.vimeo.com/video/824804225",
+    "https://player.vimeo.com/video/824804225",
+    "https://player.vimeo.com/video/824804225",
+    "https://player.vimeo.com/video/824804225",
+    "https://player.vimeo.com/video/824804225",
+  ];
+
+  return (
+    <div className="p-10  h-screen text-center">
+      <h1 className="text-3xl  mb-3">Video Slider</h1>
+      <VideoCarousel videos={videos} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import Slider from "react-slick";
+import Modal from "react-modal";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import video from "../video.png";
+import "../index.css";
+
+const VideoCarousel = ({ videos }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+
+  const settings = {
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
+
+  const openModal = (index) => {
+    setCurrentVideoIndex(index);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <Slider {...settings}>
+        {videos.map((videoUrl, index) => (
+          <div key={index} onClick={() => openModal(index)}>
+            <img src={video} alt={`video-${index}`} />
+          </div>
+        ))}
+      </Slider>
+      <Modal
+        className="fixed bg-black/70 right-0 top-0 left-0 bottom-0"
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        contentLabel="Video Modal"
+      >
+        <div className="video-container">
+          <iframe
+            title="video"
+            src={videos[currentVideoIndex]}
+            width="100%"
+            height="700px"
+            className="mt-3"
+            allow="autoplay; fullscreen"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="pagination">
+          {videos.map((paginationVideo, index) => (
+            <button
+              key={index}
+              className={`pagination-dot ${
+                index === currentVideoIndex ? "active" : ""
+              }`}
+              onClick={() => openModal(index)}
+            />
+          ))}
+        </div>
+        <button
+          onClick={closeModal}
+          className="absolute top-3 right-20 text-[30px] text-white"
+        >
+          X
+        </button>
+      </Modal>
+    </div>
+  );
+};
+
+export default VideoCarousel;
